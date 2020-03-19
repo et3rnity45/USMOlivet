d6d8f24dbd76c0c36aaeb6917adb399bd2c357e0
et3rnity45 <quentinlgr45160@gmail.com>
2020-03-19 15:42:13 +0100

    first commit

---
+++ src/main/java/com/ttolivet/usmolivet/UsmolivetApplication.java
@@ -0,0 +1,13 @@
+package com.ttolivet.usmolivet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsmolivetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsmolivetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttolivet/usmolivet/UsmolivetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttolivet.usmolivet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsmolivetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
